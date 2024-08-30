958d9c5bbd12c74aee9e18a66b1b486320ee3c4c
VladyslavSiverskiy <v.siverskiy@gmail.com>
2024-08-30 16:48:24 +0300

    before maniak modifying

--- src/main/java/com/vsiverskyi/controllers/VotingController.java
+++ src/main/java/com/vsiverskyi/controllers/VotingController.java
@@ -1078,6 +1078,7 @@ public class VotingController implements Initializable, DisplayedPlayersControll
                 res += "\nТакож вибувають: ";
                 for (String nickname : markedByBombNicknames) {
                     res += nickname + "; ";
+
                 }
             }
         }
